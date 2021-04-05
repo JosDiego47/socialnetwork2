31be05cbdf7958d713d125e90bfe9ccd0532975e
JosDiego47 <81988584+JosDiego47@users.noreply.github.com>
2021-04-05 17:01:19 -0300

    Add files via upload

---
+++ assets/includes/config.php
@@ -0,0 +1,25 @@
+<?php
+
+// MySQL Hostname / Server (for eg: 'localhost')
+$sql_host = 'localhost';
+
+
+// MySQL Database Name
+$sql_name = 'socialkit';
+
+
+// MySQL Database User
+$sql_user = 'root';
+
+
+// MySQL Database Password
+$sql_pass = 'pass';
+
+
+// Site URL
+$site_url = 'http://localhost/rehan/archives/SocialKit%20Updates/v1.2/script/Script'; // You must write "http://"
+
+
+// Facebook Implementation
+$fb_app_id = 'Your Facebook App ID'; // See this video tutorial: http://youtu.be/qBP8jmuv-GE
+$fb_app_secret = 'Your Facebook App Secret';
